58e2395caea4b4c28d7e1dddcbbdbd4160a537ee
Sean Moriarity <smoriarity.5@gmail.com>
2020-03-20 16:03:07 -0400

    penalty functions

--- lib/genex/tools/evaluation/penalty.ex
+++ lib/genex/tools/evaluation/penalty.ex
@@ -1,4 +1,27 @@
 defmodule Genex.Tools.Evaluation.Penalty do
-  def delta, do: :ok
-  def closest_valid, do: :ok
+  alias Genex.Types.Chromosome
+
+  def delta(individual, feasibility, delta, distance \\ fn _ -> 0 end) do
+    valid? = feasibility.(individual)
+    if valid? do
+      individual
+    else
+      f_penalty =
+        individual.genes
+        |> Enum.reduce(delta, fn x, acc -> acc - distance.(x) end)
+      %Chromosome{individual | fitness: f_penalty}
+    end
+  end
+
+  def closest_valid(individual, feasibility, feasible, alpha \\ 1, distance \\ fn _, _ -> 0 end) do
+    valid? = feasibility.(individual)
+    if valid? do
+      individual
+    else
+      f_penalty =
+        individual.genes
+        |> Enum.reduce(feasible.().fitness, fn x, acc -> acc - alpha*distance.(feasible.(), x) end)
+      %Chromosome{individual | fitness: f_penalty}
+    end
+  end
 end
